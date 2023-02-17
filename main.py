a8bbccd1a9033009cef7f9a26213f479ae771f58
Paulo Andrade <pauloeduardopandrade@gmail.com>
2023-02-16 21:31:49 -0300

    Complete test regression model call

--- main.py
+++ main.py
@@ -116,13 +116,15 @@ def go(config: DictConfig):
             )
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
-
+            # Test production model from W&B
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                },
+            )
 
 if __name__ == "__main__":
     go()
